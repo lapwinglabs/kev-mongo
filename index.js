bf657d23ed13cf9fdc7c4706645761668d3f8554
Brian Dentino <brian.dentino@gmail.com>
2016-03-10 14:09:51 -0800

    add compression, ttl, drop, & upgrade to kev 0.3 compatibility

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var Promise = require('bluebird')
 var mongodb = require('mongodb')
 var seconds = require('juration').parse
-var MongoClient = mongodb.MongoClient;
-var Db = mongodb.Db;
-var Collection = mongodb.Collection;
-
-Promise.promisifyAll(Collection.prototype);
-Promise.promisifyAll(Db.prototype);
-Promise.promisifyAll(MongoClient);
+var resurrect = require('./resurrect')()
+var zlib = require('zlib')
+var globber = require('glob-to-regexp')
+var MongoClient = mongodb.MongoClient
+Promise.promisifyAll(mongodb.Collection.prototype)
+Promise.promisifyAll(mongodb.Db.prototype)
+Promise.promisifyAll(mongodb.Cursor.prototype)
+Promise.promisifyAll(MongoClient)
 
 var DEFAULT_MONGO_URL = 'mongodb://127.0.0.1:27017/kev'
 var DEFAULT_COLLECTION = 'kev'
@@ -18,37 +19,39 @@ var DATA_FIELD_KEY = "value"
 var connections = {}
 var dbs = []
 var clients = []
+var expired = (r) => r && r.expiresAt && r.expiresAt < new Date(Date.now())
 
-var KevMongo = module.exports = function KevMongo(options) {
+var KevMongo = module.exports = function KevMongo (options) {
   if (!(this instanceof KevMongo)) return new KevMongo(options)
   options = options || {}
-  var collection = this.collection = options.collection || DEFAULT_COLLECTION
+  this.collection = options.collection || DEFAULT_COLLECTION
+  if (options.ttl) options.ttl = seconds(String(options.ttl))
+
   if (options.db) {
-    var db = options.db
     this.db = Promise.resolve()
-      .then(function () { return db })
-      .then(function (db) {
+      .then(() => options.db)
+      .then((db) => {
         if (!db.createCollectionAsync) return Promise.promisifyAll(db)
         else return db
       })
   } else {
     var url = options.url || DEFAULT_MONGO_URL
-    var collection = options.collection || DEFAULT_COLLECTION
-
     if (!connections[url]) {
       connections[url] = MongoClient.connectAsync(url, options.options || {})
     }
-
     this.db = connections[url]
     this.url = url
   }
-  var self = this
-  this.storage = this.db.then(function (db) {
-    self.db = db
-    return db.createCollectionAsync(collection).catch(function (err) {
+
+  this.options = options
+  this.storage = this.db.then((db) => {
+    this.db = db
+    return db.createCollectionAsync(this.collection)
+    .catch((err) => {
       if (~err.message.indexOf('collection already exists')) return
       else throw err
-    }).then(function (collection) {
+    })
+    .then((collection) => {
       if (!~dbs.indexOf(db)) {
         clients[dbs.length] = []
         dbs.push(db)
@@ -56,92 +59,104 @@ var KevMongo = module.exports = function KevMongo(options) {
       clients[dbs.indexOf(db)].push(collection)
       return collection
     })
-  }).then(function (collection) {
+  }).then((collection) => {
+    if (!collection.createIndexAsync) collection = Promise.promisifyAll(collection)
     var index = {}
     index[ID_KEY] = 1
-    if (!collection.createIndexAsync) collection = Promise.promisifyAll(collection)
     collection.createIndex(index, { background: true })
     collection.createIndex({ expiresAt: 1 }, { background: true, expireAfterSeconds: 0 })
     return collection
   })
-  if (options.ttl) this.ttl = seconds(String(options.ttl))
 }
 
-KevMongo.prototype.put = function put (key, value, done) {
-  var query = {}
-  query[ID_KEY] = key
-
-  var update = {}
-  update[ID_KEY] = key
-  update[DATA_FIELD_KEY] = value
-  if (this.ttl !== undefined) {
-    update.expiresAt = new Date(Date.now() + this.ttl * 1000)
-  }
-
-  this.storage.then(function (collection) {
-    return collection.findAndModifyAsync(query, [], update, { upsert: true })
-  }).then(function(result) {
-    if (done) done(null, result.value ? result.value[DATA_FIELD_KEY] : null)
-  }).catch(function (err) {
-    if (done) done(err)
-  })
-}
-
-KevMongo.prototype.get = function get (key, done) {
+KevMongo.prototype.get = function get (keys, done) {
   var ttl = this.ttl
-  this.storage.then(function (collection) {
-    if (Array.isArray(key)) {
-      var query = {}
-      query[ID_KEY] = { $in: key }
-      return collection.findAsync(query).then(function (cursor) {
-        var out = {}
-        var values = cursor.toArray(function (err, values) {
-          if (err && done) return done(err)
-          if (err) throw err
-          values.forEach(function (v) {
-            if (v.expiresAt && v.expiresAt < new Date(Date.now())) return
-            out[v[ID_KEY]] = v[DATA_FIELD_KEY]
-          })
-          done && done(null, out)
-        })
-      }).catch(function (err) { done && done(err) })
-    } else {
-      var query = {}
-      query[ID_KEY] = key
-      return collection.findOneAsync(query).then(function(doc) {
-        done && !doc && done()
-        done && doc.expiresAt && doc.expiresAt < new Date(Date.now()) && done()
-        done && done(null, doc[DATA_FIELD_KEY])
-      }).catch(function (err) { done && done(err) })
+  var query = {}
+  query[ID_KEY] = { $in: keys }
+  this.storage.then((db) => db.findAsync(query))
+    .then((r) => r.toArrayAsync())
+    .filter((r) => r && !expired(r))
+    .reduce((out, v) => { out[v[ID_KEY]] = unpack(this.options.compress)(v[DATA_FIELD_KEY]); return out }, {})
+    .props()
+    .then((out) => done && done(null, out))
+    .catch((err) => done && done(err))
+}
+
+KevMongo.prototype.put = function put (keys, options, done) {
+  this.storage.then((db) => {
+    var ttl = options.ttl || this.options.ttl
+    for (key in keys) {
+      var query = { [ID_KEY]: key }
+      var update = { [ID_KEY]: key }
+      if (ttl) update.expiresAt = new Date(Date.now() + ttl * 1000)
+      keys[key] = pack(this.options.compress)(keys[key])
+        .then((v) => update[DATA_FIELD_KEY] = v)
+        .then(() => db.findOneAndReplaceAsync(query, update, { upsert: true }))
+        .then((r) => (r && r.value && !expired(r.value)) ? r.value[DATA_FIELD_KEY] : null)
+        .then(unpack(this.options.compress))
     }
+    return Promise.props(keys)
+      .then((v) => done && done(null, v))
+      .catch((e) => done && done(e))
   })
 }
 
-KevMongo.prototype.del = function del (key, done) {
-  var query = {}
-  query[ID_KEY] = key
-  this.storage.then(function (collection) {
-    return collection.findAndModifyAsync(query, [], {}, { remove: true }).then(function (result) {
-      var value = result.value ? result.value[DATA_FIELD_KEY] : null
-      if (done) done(null, value)
-    }).catch(function (err) { done && done(err) })
+KevMongo.prototype.del = function del (keys, done) {
+  this.storage.then((db) => {
+    return Promise.resolve(keys)
+      .reduce((out, key) => {
+        out[key] = db.findOneAndDeleteAsync({ [ID_KEY]: key })
+          .then((r) => (r && r.value) ? r.value[DATA_FIELD_KEY] : null)
+          .then(unpack(this.options.compress))
+        return out
+      }, {})
+      .props()
+      .then((v) => done && done(null, v))
+      .catch((e) => done && done(e))
   })
+}
+
+KevMongo.prototype.drop = function drop (pattern, done) {
+  var re = globber(pattern)
+  this.storage
+    .then((db) => db.deleteManyAsync({ [ID_KEY]: { $regex: re } }))
+    .then((r) => done && done(null, r.deletedCount))
+    .catch((e) => done && done(e))
 }
 
 KevMongo.prototype.close = function (done) {
-  var self = this
-  this.storage.then(function (collection) {
-    var db = self.db
+  this.storage.then((collection) => {
+    var db = this.db
     var db_clients = clients[dbs.indexOf(db)]
     db_clients.splice(db_clients.indexOf(collection), 1)
     if (db_clients.length === 0) {
       var index = dbs.indexOf(db)
       dbs.splice(index, 1)
       clients.splice(index, 1)
-      self.url && delete connections[self.url]
+      this.url && delete connections[this.url]
       db.close(done)
     } else {
       done && done()
     }
+  })
+}
+
+function pack (compress) {
+  return Promise.promisify((value, done) => {
+    if (!value || !compress) return setImmediate(() => done(null, value))
+    zlib.deflate(resurrect.stringify(value), compress, (err, buf) => {
+      if (err) done(err)
+      else done(null, buf.toString('base64'))
+    })
+  })
+}
+
+function unpack (compress) {
+  return Promise.promisify((value, done) => {
+    if (!value || !compress) return setImmediate(() => done(null, value))
+    zlib.inflate(new Buffer(value, 'base64'), compress, (err, val) => {
+      if (err) done(err)
+      else done(null, resurrect.resurrect(val.toString()))
+    })
   })
 }
--- test/test.js
+++ test/test.js
@@ -4,87 +4,49 @@ var KevMongo = require('../index.js')
 var assert = require('assert')
 var mongoose = require('mongoose')
 
-function run_test(kev) {
-  return kev.putAsync('key1', 'value1').then(function() {
-    return kev.getAsync('key1')
-  }).then(function(value) {
-    assert.equal(value, 'value1')
-  }).then(function () {
-    return kev.delAsync('key1')
-  }).then(function(old) {
-    assert.equal(old, 'value1')
-  }).then(function () {
-    return kev.getAsync('key1')
-  }).then(function (value) {
-    assert.equal(value, null)
-  }).then(function () {
-    var max = 2000
-    var puts = []
-    for (var i = 1; i <= max; i++) {
-      puts.push(kev.putAsync(String(i), i))
-    }
-    return Promise.all(puts)
-  }).then(function () {
-    console.log('put all')
-    var max = 2000
-    var keys = []
-    for (var i = 1; i <= max; i++) {
-      keys.push(String(i))
-    }
-    return kev.getAsync(keys)
-  }).then(function (values) {
-    console.log('got all')
-    assert.equal(values['100'], 100)
-    assert.equal(values['2000'], 2000)
-  }).then(function (values) {
-    return kev.putAsync('key2', 'to-expire')
-  }).then(function () {
-    return kev.getAsync('key2')
-  }).then(function (value) {
-    assert.equal(value, 'to-expire')
-    return Promise.delay(6000)
-  }).then(function () {
-    return kev.getAsync('key2')
-  }).then(function(value) {
-    assert.equal(value, null)
-    kev.close()
-    console.log('PASSED')
-  })
-}
+var test_core = require('kev/test/test-plugin-core')
+var test_ttl = require('kev/test/test-ttl')
 
 var MongoClient = Promise.promisifyAll(require('mongodb').MongoClient)
 MongoClient
   .connectAsync(process.env.MONGO_URL + '/kev-test2')
-  .then(function (db) {
+  .then((db) => {
     // Verify it accepts an existing mongo connection
-    return Promise.promisifyAll(Kev({ store: KevMongo({ db: db, ttl: '5 sec' }) }))
+    var store = KevMongo({ db: db, ttl: '5 sec' })
+    return test_core(store).then(() => test_ttl(store))
   })
-  .then(function (kev) {
-    return run_test(kev)
-  })
-  .then(function () {
+  .then(() => {
     // Verify it can handle more than one open mongo connection
-    var mongo = KevMongo({
+    var store = KevMongo({
       ttl: 5,
       db: mongoose
         .connect(process.env.MONGO_URL)
         .then(() => mongoose.connection.db)
     })
-    return Promise.promisifyAll(Kev({ store: mongo }))
+    return test_core(store).then(() => test_ttl(store))
   })
-  .then(function (kev) {
-    return run_test(kev)
-  })
-  .then(function () {
-    var mongo = KevMongo( { url: process.env.MONGO_URL + '/kev-test', ttl: 5 } )
-    var kev = Promise.promisifyAll(Kev({ store: mongo }))
+  .then(() => {
     // Verify it accepts urls
-    run_test(kev)
+    var store = KevMongo({ url: process.env.MONGO_URL + '/kev-test', ttl: 5 })
+    return test_core(store).then(() => test_ttl(store))
   })
-  .then(function () {
+  .then(() => {
     // Verify url pooling
-    var mongo1 = KevMongo( { url: process.env.MONGO_URL + '/kev-test', ttl: 5 } )
-    var mongo2 = KevMongo( { url: process.env.MONGO_URL + '/kev-test', ttl: 5 } )
-    assert.equal(mongo1.db, mongo2.db)
-    mongo1.close(() => mongo2.close())
+    var mongo1 = KevMongo({ url: process.env.MONGO_URL + '/kev-test', ttl: 5 })
+    var mongo2 = KevMongo({ url: process.env.MONGO_URL + '/kev-test', ttl: 5 })
+    return mongo1.db.then(() => mongo2.db)
+      .delay(6000)
+      .then(() => {
+        assert.equal(mongo1.db, mongo2.db)
+        mongo1.close(() => mongo2.close())
+      })
+  })
+  .then(() => {
+    // Verify compression
+    var store = KevMongo({
+      url: process.env.MONGO_URL + '/kev-compressed',
+      ttl: 5,
+      compress: true
+    })
+    return test_core(store).then(() => test_ttl(store))
   })
